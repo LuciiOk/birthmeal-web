312333b3ef6e0db8dba971b2f19244b95c40a703
Luciano Portales <lportales655@gmail.com>
2022-10-29 00:33:35 -0300

    updated some issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import resolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3200,
-  }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        resolve({
+          browser: true,
+          preferBuiltins: false,
+        }),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+    // scss resolve
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],    
+  },
 })
