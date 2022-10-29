535099e7c38b030c523259af7d1ae0034d473a3e
Luciano Portales <lportales655@gmail.com>
2022-10-28 23:55:14 -0300

    updated node version

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3200,
+  },
+  // `build.rollupOptions.external` react-hot-toast
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-router-dom', 'react-hot-toast'],
+    },
   }
 })
