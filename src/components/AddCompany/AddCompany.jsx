d60851f56ede6e39a779747d20ed72f5274e8098
Luciano Portales <lportales655@gmail.com>
2022-10-31 18:00:10 -0300

    added validation

--- src/components/AddCompany/AddCompany.jsx
+++ src/components/AddCompany/AddCompany.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFormState } from "react-hook-form";
 import "./AddCompany.scss";
 import AxiosInstance from "../../utils/AxiosIntance";
 import LocationsStep from "./LocationStep";
 
 const AddCompany = ({ onSubmit, dataEdit }) => {
   const [step, setStep] = useState(1);
-  const { register, handleSubmit, errors } = useForm({
-    defaultValues: dataEdit,
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, ...formState },
+  } = useForm({
+    defaultValues: {
+      name: dataEdit?.name,
+      webUrl: dataEdit?.webUrl,
+      category: dataEdit?.category._id,
+      description: dataEdit?.description,
+    },
+    mode: "all",
   });
   const [locations, setLocations] = useState([]);
-
-  const onSubmitData = (data) => {
-    onSubmit(data);
-  };
 
   const onNextStep = () => {
     setStep(step + 1);
@@ -21,6 +27,12 @@ const AddCompany = ({ onSubmit, dataEdit }) => {
 
   const onPrevStep = () => {
     setStep(step - 1);
+  };
+
+  const onSubmitData = (data) => {
+    if (formState.isValid) {
+      onSubmit(data, locations);
+    }
   };
 
   return (
@@ -69,6 +81,10 @@ const FormStep = ({ register, errors }) => {
     getCategories();
   }, []);
 
+  if (!categories.length) {
+    return null;
+  }
+
   return (
     <React.Fragment>
       <div className="addCompany__form__group">
@@ -82,6 +98,9 @@ const FormStep = ({ register, errors }) => {
           className="form-input"
           {...register("name", { required: true })}
         />
+        {errors.name && (
+          <span className="form-error">El nombre es requerido</span>
+        )}
       </div>
       <div className="addCompany__form__group">
         <label htmlFor="webUrl" className="form-label">
@@ -94,6 +113,9 @@ const FormStep = ({ register, errors }) => {
           className="form-input"
           {...register("webUrl", { required: true })}
         />
+        {errors.webUrl && (
+          <span className="form-error">La url es requerida</span>
+        )}
       </div>
       <div className="addCompany__form__group">
         <label htmlFor="category" className="form-label">
@@ -112,6 +134,9 @@ const FormStep = ({ register, errors }) => {
             </option>
           ))}
         </select>
+        {errors.category && (
+          <span className="form-error">La categoría es requerida</span>
+        )}
       </div>
       <div className="addCompany__form__group">
         <label htmlFor="description" className="form-label">
@@ -123,6 +148,9 @@ const FormStep = ({ register, errors }) => {
           className="form-input"
           {...register("description", { required: true })}
         />
+        {errors.description && (
+          <span className="form-error">La descripción es requerida</span>
+        )}
       </div>
     </React.Fragment>
   );
