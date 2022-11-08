735522ff513783573a3fcf895212043d331d8a7e
Luciano Portales <lportales655@gmail.com>
2022-11-08 19:20:14 -0300

    go to admin

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -2,21 +2,32 @@ import React from "react";
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 const NavBar = () => {
+  const { isAuth } = React.useContext(AuthContext);
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar">
       <ul className="navbar__list">
         <li className="navbar__item">
-          <Link to="#about" className="navbar__link" >
+          <Link to="#about" className="navbar__link">
             Sobre nosotros
           </Link>
         </li>
         <li className="navbar__item">
-          <Link to="#contact" className="navbar__link" >
+          <Link to="#contact" className="navbar__link">
             Contacto
           </Link>
         </li>
       </ul>
+      {isAuth && (
+        <button className="navbar__button" onClick={() => navigate("/admin")}>
+          Admin
+        </button>
+      )}
     </nav>
   );
 };
