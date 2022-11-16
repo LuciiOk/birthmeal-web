167976fc811fdc8fe5713560fa77eca03ff8da4a
Luciano Portales <lportales655@gmail.com>
2022-11-16 19:32:06 -0300

    boundary error

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import AuthProvider from "./contexts/AuthContext";
 import Routes from "./routes/Routes";
 import { Toaster } from "react-hot-toast";
 import "./App.scss";
+import BoundaryError from "./errors/ErrorBoundary";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes />
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        // allow to dismiss toast by clicking on it
-        toastOptions={{
-          style: {
-            background: "#363636",
-            color: "#fff",
-            fontSize: "14px",
-          },
-          // Define default options
-          duration: 1500,
-        }}
-      />
-    </AuthProvider>
+    <BoundaryError>
+      <AuthProvider>
+        <Routes />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          // allow to dismiss toast by clicking on it
+          toastOptions={{
+            style: {
+              background: "#363636",
+              color: "#fff",
+              fontSize: "14px",
+            },
+            // Define default options
+            duration: 1500,
+          }}
+        />
+      </AuthProvider>
+    </BoundaryError>
   );
 }
 
---
+++ src/errors/ErrorBoundary.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './ErrorBoundary.scss';
+
+class BoundaryError extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="container main__container">
+          <h2>
+            Oh, no!, ha ocurrido un error.<br /> Por favor, vuelva a intentarlo más
+            tarde.
+          </h2>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default BoundaryError;
