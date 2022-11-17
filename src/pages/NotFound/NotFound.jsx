d1dcf257ca3ee0d90d280c148bf3d610ebba0e7c
Luciano Portales <lportales655@gmail.com>
2022-11-17 00:25:32 -0300

    404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './NotFound.scss';
+import img404 from "../../assets/images/Burger-404.png";
 
 const NotFound = () => {
   return (
     <div className="notfound__container">
+      <img src={img404} alt="404" className="notfound__img" />
       <h2>404</h2>
       <h3>Lo sentimos, la página que buscas no existe.</h3>
     </div>
